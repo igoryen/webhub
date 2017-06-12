6eaac6915679e25912fa51b7dce9804ababfb9eb
igoryen <igor.yentaltsev@gmail.com>
2017-06-12 01:17:47 -0400

    Added events

--- webhub.js
+++ webhub.js
@@ -592,31 +592,31 @@ window.onload = function () {
         },
         {
             type: "",
-            name: "",
-            link: "",
-            cat:  "webdesign",
-            desc: ""
+            name: "War's Timeline",
+            link: "https://ru.wikipedia.org/wiki/%D0%A5%D1%80%D0%BE%D0%BD%D0%B8%D0%BA%D0%B0_%D0%92%D0%B5%D0%BB%D0%B8%D0%BA%D0%BE%D0%B9_%D0%9E%D1%82%D0%B5%D1%87%D0%B5%D1%81%D1%82%D0%B2%D0%B5%D0%BD%D0%BD%D0%BE%D0%B9_%D0%B2%D0%BE%D0%B9%D0%BD%D1%8B",
+            cat:  "war",
+            desc: "month by month"
         },
         {
             type: "",
-            name: "",
-            link: "",
-            cat:  "webdesign",
-            desc: ""
+            name: "Victors",
+            link: "http://www.pobediteli.ru/",
+            cat:  "war",
+            desc: "Multimedia map of the War"
         },
         {
             type: "",
-            name: "",
-            link: "",
-            cat:  "webdesign",
-            desc: ""
+            name: "Moment",
+            link: "https://momentjs.com/",
+            cat:  "programming",
+            desc: "JS library for dates"
         },
         {
             type: "",
-            name: "",
-            link: "",
-            cat:  "webdesign",
-            desc: ""
+            name: "Komsomol Truth",
+            link: "http://kpcanada.press/",
+            cat:  "news",
+            desc: "in Canada"
         },
         {
             type: "",
